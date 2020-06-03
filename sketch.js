afdbad88c4b17c502dd6f254b12e6232e9e64611
vihaantalegaonkar <65075590+vihaantalegaonkar@users.noreply.github.com>
2020-06-03 18:19:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -37,11 +37,12 @@ function draw() {
   ground.display();
 }
 
-function keyPressed() {
-	if (keyCode === UP_ARROW) {
-
-	  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-  
+function keyPressed(){
+	if(keyCode == UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{
+		x:1000,
+		y:600
+		});
 	}
 }
 
